35924b953dc1942831646a9194aca5759aa0692d
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2024-05-31 00:17:25 +0530

    file update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chest-Disease-Classification-from-Chest-CT-Scan-Image"
+REPO_NAME = "Chest_Cancer_Classification"
 AUTHOR_USER_NAME = "RishavMishraRM"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "rishav.mishra.rkm@gmail.com"
